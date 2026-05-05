ac0d73c71460652aea77c3ca7972c6853ac22f7b
MIDHUN TR <midhuntr7356@gmail.com>
2026-05-05 18:50:37 +0530

    feat: implement login page UI components including AuthInput, AuthCard, and password validator

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@ import PasswordValidator from "@/components/ui/AuthPassword"
 export default function Login(){
     return(
         <>
-            <PasswordValidator label="text" name="Login Into your Account"/>
+            <PasswordValidator label="Password" name="Enter Your password"/>
         <p>Hello this is the login page </p>
         </>
     )
