b253a83baef4e8787bacf05b26c1789e81fb5132
MIDHUN TR <midhuntr7356@gmail.com>
2026-07-06 22:27:17 +0530

    feat: enhance product fetching response with success message

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
     try{
         const products = await prisma.product.findMany();
-        return NextResponse.json(products);
+        return NextResponse.json({message: "Products fetched successfully", products}, { status: 200 });
     }
     catch(e){
         return NextResponse.json({ error: "Failed to fetch products" }, { status: 500 });
